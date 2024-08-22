982bbb4ca97f1f97fd8754fdfb2631b6442a6d55
양희연 <huiyeony888@gmail.com>
2024-08-22 20:02:21 +0900

    2.6 스타일링

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import style from "@/styles/Index.module.css";
 export default function Page() {
   const router = useRouter();
 
-  return <div>search 페이지 입니다</div>;
+  return <div className={style.div}>search 페이지 입니다</div>;
 }
